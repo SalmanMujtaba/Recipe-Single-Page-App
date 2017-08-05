f259d153c39958a933ec93353ec93e7a031ace7d
salman mujtaba <salmanmujtaba1793@gmail.com>
2017-08-05 18:09:10 -0400

    Linked components via event and property binding

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../../recipe.model';
 
 @Component({
@@ -7,10 +7,22 @@ import { Recipe } from '../../recipe.model';
   styleUrls: ['./recipe-item.component.scss']
 })
 export class RecipeItemComponent implements OnInit {
+  //To link recipe-list and recipe-detail. Recipe-item is receiving data from recipe-list.
   @Input() recipe: Recipe;
+
+  // Event to inform recipe detail about which recipe was clicked.
+  @Output() loadRecipe = new EventEmitter<void>();
+
   constructor() { }
 
   ngOnInit() {
   }
 
+// We're not passing the recipe here 'cause the parent component has a for
+// loop which loops over individual recipe. And it is not the immediate parent which
+//needs the recipe selected data, but recipes-component.
+
+  onRecipeSelect(){
+    this.loadRecipe.emit();    
+  }
 }
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 import { Recipe } from '../recipe.model';
+
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
@@ -8,16 +10,25 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
 
+//There is a chain of events here. This particular event informs recipe-components
+//about the component selected
+
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
   recipes: Recipe[] = [
     new Recipe('Test Recipe', 'simple desciption', 
     'https://s-media-cache-ak0.pinimg.com/originals/f7/fb/04/f7fb04e865fdc9900f4476364b9292e3.jpg')
   ,
-   new Recipe('Test Recipe', 'simple desciption', 
+   new Recipe('Second Recipe', 'Second sample desciption', 
     'https://s-media-cache-ak0.pinimg.com/originals/f7/fb/04/f7fb04e865fdc9900f4476364b9292e3.jpg')
  ];
   constructor() { }
 
   ngOnInit() {
+  }
+  onRecipeSelected(recipe: Recipe){
+    this.recipeWasSelected.emit(recipe);
+
   }
 
 }
